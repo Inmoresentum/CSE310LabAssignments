6f5a28ede3654f0ce328f17bae744819215c0f6a
Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>
2023-10-06 05:14:31 +0600

    fix: fixed overDraftLimit issue for withdrawal in case of CheckingAccount

--- src/main/java/org/cse310/bracu/assignment1/CheckingAccount.java
+++ src/main/java/org/cse310/bracu/assignment1/CheckingAccount.java
@@ -1,10 +1,13 @@
 package org.cse310.bracu.assignment1;
 
+import java.time.LocalDateTime;
+
 public class CheckingAccount extends BankAccount {
     private double overDraftLimit;
 
     public CheckingAccount(Integer accountNumber, String accountName, double balance, double overDraftLimit) {
         super(accountNumber, accountName);
+        super.setBalance(balance);
         this.overDraftLimit = overDraftLimit;
     }
 
@@ -14,5 +17,21 @@ public class CheckingAccount extends BankAccount {
 
     public void setOverDraftLimit(double overDraftLimit) {
         this.overDraftLimit = overDraftLimit;
+    }
+
+    @Override
+    public void withdraw(double amount) throws Exception {
+        if (amount < 100) {
+            throw new Exception("Minimum withdraw amount is 100");
+        }
+        if (this.getBalance() - amount + overDraftLimit < 500.0) {
+            throw new Exception("Balance Can't go below 500");
+        }
+        this.setBalance(this.getBalance() - amount);
+        var transaction = new Transaction(LocalDateTime.now(),
+                amount,
+                TransactionType.DEPOSIT,
+                this.getBalance());
+        this.getListOfTransactions().add(transaction);
     }
 }

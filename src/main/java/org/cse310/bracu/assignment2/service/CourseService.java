21cb0236fb9db4268c3acdafc7fefdaff4c04fc8
Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>
2023-10-14 00:50:53 +0600

    refactor: refactored the CourseService.java and StudentService.java

--- src/main/java/org/cse310/bracu/assignment2/service/CourseService.java
+++ src/main/java/org/cse310/bracu/assignment2/service/CourseService.java
@@ -15,59 +15,59 @@ public class CourseService {
 
     public void addCourse(Course course) throws SQLException {
         String sql = "INSERT INTO Course (courseID, courseCode, totalCapacity, currentNumber, version) VALUES (?, ?, ?, ?, ?)";
-        try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
-            pstmt.setString(1, course.getCourseID());
-            pstmt.setString(2, course.getCourseCode());
-            pstmt.setInt(3, course.getTotalCapacity());
-            pstmt.setInt(4, course.getCurrentNumber());
-            pstmt.setInt(5, course.getVersion());
-            pstmt.executeUpdate();
+        try (PreparedStatement prepareStatement = connection.prepareStatement(sql)) {
+            prepareStatement.setString(1, course.getCourseID());
+            prepareStatement.setString(2, course.getCourseCode());
+            prepareStatement.setInt(3, course.getTotalCapacity());
+            prepareStatement.setInt(4, course.getCurrentNumber());
+            prepareStatement.setInt(5, course.getVersion());
+            prepareStatement.executeUpdate();
         }
     }
 
     public void removeCourse(String courseId) throws SQLException {
         String sql = "DELETE FROM Course WHERE courseID = ?";
-        try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
-            pstmt.setString(1, courseId);
-            pstmt.executeUpdate();
+        try (PreparedStatement prepareStatement = connection.prepareStatement(sql)) {
+            prepareStatement.setString(1, courseId);
+            prepareStatement.executeUpdate();
         }
     }
 
     public void updateCourse(Course course) throws SQLException {
         String sql = "UPDATE Course SET courseCode = ?, totalCapacity = ?, currentNumber = ?, version = ? WHERE courseID = ?";
-        try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
-            pstmt.setString(1, course.getCourseCode());
-            pstmt.setInt(2, course.getTotalCapacity());
-            pstmt.setInt(3, course.getCurrentNumber());
-            pstmt.setInt(4, course.getVersion());
-            pstmt.setString(5, course.getCourseID());
-            pstmt.executeUpdate();
+        try (PreparedStatement prepareStatement = connection.prepareStatement(sql)) {
+            prepareStatement.setString(1, course.getCourseCode());
+            prepareStatement.setInt(2, course.getTotalCapacity());
+            prepareStatement.setInt(3, course.getCurrentNumber());
+            prepareStatement.setInt(4, course.getVersion());
+            prepareStatement.setString(5, course.getCourseID());
+            prepareStatement.executeUpdate();
         }
     }
 
     public void addLecturerToCourse(String courseId, String lecturerId) throws SQLException {
         String sql = "INSERT INTO Lecturer_Course (lecturerId, courseId) VALUES (?, ?)";
-        try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
-            pstmt.setString(1, lecturerId);
-            pstmt.setString(2, courseId);
-            pstmt.executeUpdate();
+        try (PreparedStatement prepareStatement = connection.prepareStatement(sql)) {
+            prepareStatement.setString(1, lecturerId);
+            prepareStatement.setString(2, courseId);
+            prepareStatement.executeUpdate();
         }
     }
 
     public void removeLecturerFromCourse(String courseId, String lecturerId) throws SQLException {
         String sql = "DELETE FROM Lecturer_Course WHERE lecturerId = ? AND courseId = ?";
-        try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
-            pstmt.setString(1, lecturerId);
-            pstmt.setString(2, courseId);
-            pstmt.executeUpdate();
+        try (PreparedStatement prepareStatement = connection.prepareStatement(sql)) {
+            prepareStatement.setString(1, lecturerId);
+            prepareStatement.setString(2, courseId);
+            prepareStatement.executeUpdate();
         }
     }
 
     public List<Course> findAllCourses() throws SQLException {
         List<Course> courses = new ArrayList<>();
         String sql = "SELECT * FROM Course";
-        try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
-            ResultSet rs = pstmt.executeQuery();
+        try (PreparedStatement prepareStatement = connection.prepareStatement(sql)) {
+            ResultSet rs = prepareStatement.executeQuery();
             while (rs.next()) {
                 Course course = new Course(rs.getString("courseID"), rs.getString("courseCode"),
                         rs.getInt("totalCapacity"), rs.getInt("currentNumber"),
@@ -79,4 +79,3 @@ public class CourseService {
         return courses;
     }
 }
-
--- src/main/java/org/cse310/bracu/assignment2/service/StudentService.java
+++ src/main/java/org/cse310/bracu/assignment2/service/StudentService.java
@@ -16,29 +16,29 @@ public class StudentService {
 
     public void addStudent(Student student) throws SQLException {
         String sql = "INSERT INTO User (userId, name, email, encryptedPassword, userType) VALUES (?, ?, ?, ?, ?)";
-        try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
-            pstmt.setString(1, student.getUserId());
-            pstmt.setString(2, student.getName());
-            pstmt.setString(3, student.getEmail());
-            pstmt.setString(4, student.getEncryptedPassword());
-            pstmt.setString(5, UserType.STUDENT.name());
-            pstmt.executeUpdate();
+        try (PreparedStatement prepareStatement = connection.prepareStatement(sql)) {
+            prepareStatement.setString(1, student.getUserId());
+            prepareStatement.setString(2, student.getName());
+            prepareStatement.setString(3, student.getEmail());
+            prepareStatement.setString(4, student.getEncryptedPassword());
+            prepareStatement.setString(5, UserType.STUDENT.name());
+            prepareStatement.executeUpdate();
         }
 
         sql = "INSERT INTO Student (userId, studentId, version) VALUES (?, ?, ?)";
-        try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
-            pstmt.setString(1, student.getUserId());
-            pstmt.setString(2, student.getStudentID());
-            pstmt.setInt(3, student.getVersion());
-            pstmt.executeUpdate();
+        try (PreparedStatement prepareStatement = connection.prepareStatement(sql)) {
+            prepareStatement.setString(1, student.getUserId());
+            prepareStatement.setString(2, student.getStudentID());
+            prepareStatement.setInt(3, student.getVersion());
+            prepareStatement.executeUpdate();
         }
     }
 
     public Optional<Student> findStudentById(String userId) throws SQLException {
         String sql = "SELECT * FROM User JOIN Student ON User.userId = Student.userId WHERE User.userId = ?";
-        try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
-            pstmt.setString(1, userId);
-            ResultSet rs = pstmt.executeQuery();
+        try (PreparedStatement prepareStatement = connection.prepareStatement(sql)) {
+            prepareStatement.setString(1, userId);
+            ResultSet rs = prepareStatement.executeQuery();
             if (rs.next()) {
                 var student = new Student(rs.getString("userId"), rs.getString("name"), rs.getString("email"),
                         rs.getString("encryptedPassword"), rs.getString("studentId"),
@@ -52,9 +52,9 @@ public class StudentService {
 
     public Optional<Student> findStudentByEmail(String email) throws SQLException {
         String sql = "SELECT * FROM User JOIN Student ON User.userId = Student.userId WHERE User.email = ?";
-        try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
-            pstmt.setString(1, email);
-            ResultSet rs = pstmt.executeQuery();
+        try (PreparedStatement prepareStatement = connection.prepareStatement(sql)) {
+            prepareStatement.setString(1, email);
+            ResultSet rs = prepareStatement.executeQuery();
             if (rs.next()) {
                 var student = new Student(rs.getString("userId"), rs.getString("name"), rs.getString("email"),
                         rs.getString("encryptedPassword"), rs.getString("studentId"),
@@ -68,9 +68,9 @@ public class StudentService {
 
     public Optional<Student> findStudentByStudentId(String studentId) throws SQLException {
         String sql = "SELECT * FROM User JOIN Student ON User.userId = Student.userId WHERE Student.studentId = ?";
-        try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
-            pstmt.setString(1, studentId);
-            ResultSet rs = pstmt.executeQuery();
+        try (PreparedStatement prepareStatement = connection.prepareStatement(sql)) {
+            prepareStatement.setString(1, studentId);
+            ResultSet rs = prepareStatement.executeQuery();
             if (rs.next()) {
                 var student = new Student(rs.getString("userId"), rs.getString("name"), rs.getString("email"),
                         rs.getString("encryptedPassword"), rs.getString("studentId"),
@@ -85,9 +85,9 @@ public class StudentService {
     private Set<Course> getCoursesByStudentId(String studentId) throws SQLException {
         Set<Course> courses = new HashSet<>();
         String sql = "SELECT Course.* FROM Course JOIN Student_Course ON Course.courseID = Student_Course.courseId WHERE Student_Course.studentId = ?";
-        try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
-            pstmt.setString(1, studentId);
-            ResultSet rs = pstmt.executeQuery();
+        try (PreparedStatement prepareStatement = connection.prepareStatement(sql)) {
+            prepareStatement.setString(1, studentId);
+            ResultSet rs = prepareStatement.executeQuery();
             while (rs.next()) {
                 Course course = new Course(rs.getString("courseID"), rs.getString("courseCode"),
                         rs.getInt("totalCapacity"), rs.getInt("currentNumber"),
@@ -101,8 +101,8 @@ public class StudentService {
     public List<Student> findAllStudents() throws SQLException {
         List<Student> students = new ArrayList<>();
         String sql = "SELECT * FROM User JOIN Student ON User.userId = Student.userId";
-        try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
-            ResultSet rs = pstmt.executeQuery();
+        try (PreparedStatement prepareStatement = connection.prepareStatement(sql)) {
+            ResultSet rs = prepareStatement.executeQuery();
             while (rs.next()) {
                 var student = new Student(rs.getString("userId"), rs.getString("name"), rs.getString("email"),
                         rs.getString("encryptedPassword"), rs.getString("studentId"),
@@ -116,37 +116,37 @@ public class StudentService {
 
     public void updateStudentName(String userId, String newName) throws SQLException {
         String sql = "UPDATE User SET name = ? WHERE userId = ?";
-        try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
-            pstmt.setString(1, newName);
-            pstmt.setString(2, userId);
-            pstmt.executeUpdate();
+        try (PreparedStatement prepareStatement = connection.prepareStatement(sql)) {
+            prepareStatement.setString(1, newName);
+            prepareStatement.setString(2, userId);
+            prepareStatement.executeUpdate();
         }
     }
 
     public void updateStudentEmail(String userId, String newEmail) throws SQLException {
         String sql = "UPDATE User SET email = ? WHERE userId = ?";
-        try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
-            pstmt.setString(1, newEmail);
-            pstmt.setString(2, userId);
-            pstmt.executeUpdate();
+        try (PreparedStatement prepareStatement = connection.prepareStatement(sql)) {
+            prepareStatement.setString(1, newEmail);
+            prepareStatement.setString(2, userId);
+            prepareStatement.executeUpdate();
         }
     }
 
     public void addCourseToStudent(String studentId, String courseId) throws SQLException {
         String sql = "INSERT INTO Student_Course (studentId, courseId) VALUES (?, ?)";
-        try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
-            pstmt.setString(1, studentId);
-            pstmt.setString(2, courseId);
-            pstmt.executeUpdate();
+        try (PreparedStatement prepareStatement = connection.prepareStatement(sql)) {
+            prepareStatement.setString(1, studentId);
+            prepareStatement.setString(2, courseId);
+            prepareStatement.executeUpdate();
         }
     }
 
     public void removeCourseFromStudent(String studentId, String courseId) throws SQLException {
         String sql = "DELETE FROM Student_Course WHERE studentId = ? AND courseId = ?";
-        try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
-            pstmt.setString(1, studentId);
-            pstmt.setString(2, courseId);
-            pstmt.executeUpdate();
+        try (PreparedStatement prepareStatement = connection.prepareStatement(sql)) {
+            prepareStatement.setString(1, studentId);
+            prepareStatement.setString(2, courseId);
+            prepareStatement.executeUpdate();
         }
     }
 }

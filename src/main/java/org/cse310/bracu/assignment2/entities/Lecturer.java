02bec7dbcbbac55bfc47be79c7cdfe14a29b47d3
Inmoresentum <noor.mohammad.rafee@g.bracu.ac.bd>
2023-10-18 09:35:45 +0600

    refactor: :recycle:

--- src/main/java/org/cse310/bracu/assignment2/entities/Lecturer.java
+++ src/main/java/org/cse310/bracu/assignment2/entities/Lecturer.java
@@ -6,7 +6,9 @@ public class Lecturer extends User {
     private final String lecturerId;
     private Set<Course> courses;
 
-    public Lecturer(String userId, String name, String email, String encryptedPassword, String lecturerId, Set<Course> courses) {
+    public Lecturer(String userId, String name,
+                    String email, String encryptedPassword,
+                    String lecturerId, Set<Course> courses) {
         super(userId, name, email, encryptedPassword, UserType.LECTURER);
         this.lecturerId = lecturerId;
         this.courses = courses;
